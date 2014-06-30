5bcd9da604c0c25995b889a9efbed6b539efe4b8
Amanpreet Singh <apsdehal@gmail.com>
2014-06-30 19:44:54 +0530

    Adds css to page

--- app/views/home.php
+++ app/views/home.php
@@ -5,9 +5,9 @@
 	<link rel="stylesheet" type="text/css" href="/assets/css/main.css">
 </head>
 <body>
-<p class="info">You must login to WikiMedia before pushing annotations</p>
-<a title="You must login before pushing" href="//tools.wmflabs.org/wikidata-annotation-tool?action=authorize"><button>Login to WikiMedia</button></a>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<p class="info">You must login to Wikimedia before pushing annotations</p>
+<a title="You must login before pushing" href="//tools.wmflabs.org/wikidata-annotation-tool?action=authorize" class="login"><button>Login to Wikimedia</button></a>
+<!-- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script> -->
 <script>window.jQuery || document.write('<script src="/assets/js/jquery.min.js"><\/script>')</script>
 <script src="/assets/js/main.js"></script>
 </body>
