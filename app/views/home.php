f3a69388b8be501705b9d3bc12f0bfa4f142b484
Amanpreet Singh <apsdehal@gmail.com>
2014-06-30 16:26:20 +0530

    Adds view model

---
+++ app/views/home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Push to Wikidata</title>
+	<link rel="stylesheet" type="text/css" href="/assets/css/main.css">
+</head>
+<body>
+<p class="info">You must login to WikiMedia before pushing annotations</p>
+<a title="You must login before pushing" href="//tools.wmflabs.org/widar/index.php?action=authorize"><button>Login to WikiMedia</button></a>
+<script src="//cdnjs.cloudflare.com/ajax/libs/zepto/1.1.3/zepto.min.js"></script>
+<script>window.Zepto || document.write('<script src="/assets/js/zepto.min.js"><\/script>')</script>
+<script src="/assets/js/main.js"></script>
+</body>
+</html>
