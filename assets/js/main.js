0a95512dca7d870ba9285a62166dca2c9e7cfd4b
Amanpreet Singh <apsdehal@gmail.com>
2014-07-01 03:23:04 +0530

    Shifts config to a global variable

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,46 +1,47 @@
-/* Creating a global object */
+/* Create a config object */
 
+var config = {
+	api_root: "http://tools.wmflabs.org/widar"
+}
+
+/* Creating a global object */
 var Bajo = {
 	/* Variable for storing config */
-	config: null,
+	config: config,
 	
 	/**
 	 *	Helper for setting the config variable, gets config,json and sets it to config
 	 */	
 	setConfig: function(){
-		var self = this;
-		$.getJSON('config/config.json', function(data){
-			self.config = data;
-			self.oauthTimeout = window.setTimeout(self.checkOauthStatus, 1000);
-		});
+		this.oauthTimeout = window.setTimeout(self.checkOauthStatus, 1000);
 	},
 	checkOauthStatus: function () {
-	var self = this;	
-	$.getJSON ( self.config.api_root , {
-		action:'get_rights',
-		botmode:1
-	} , function ( d ) {
-		var h = '' ;
-		if ( d.error != 'OK' || typeof (d.result||{}).error != 'undefined' ) {
-			h += "<div><a title='You need to authorise WiDaR to edit" 
-			  + "on your behalf if you want this tool to edit Wikidata.' target='_blank' href='/widar/index.php?action=authorize'>WiDaR</a><br/>not authorised.</div>" ;
-		} else {
-			h += "<div>Logged into <a title='WiDaR authorised' target='_blank'" 
-			  +  "href='/widar/'>WiDaR</a> as <span class='username'>" + d.result.query.userinfo.name + "</span></div>" ;
-			$.each ( d.result.query.userinfo.groups , function ( k , v ) {
-				if ( v != 'bot' ) return ;
-				h += "<div><b>You are a bot</b>, no throttling for you!</div>" ;
-			} ) ;
-			window.clearTimeout(self.oauthTimeout);
-		}
-		$('.oauth_status').html ( h ) ;
-		// $.each ( (((((d||{}).result||{}).query||{}).userinfo||{}).groups||[]) , function ( k , v ) {
-		// 	if ( v == 'bot' ) {
-		// 		max_widar_concurrent = 5 ;
-		// 		widar_edit_delay = 1 ;
-		// 	}
-		// } ) ;
-	} ) ;
+		var self = this;	
+		$.getJSON ( self.config.api_root , {
+			action:'get_rights',
+			botmode:1
+		}, function ( d ) {
+			var h = '' ;
+			if ( d.error != 'OK' || typeof (d.result||{}).error != 'undefined' ) {
+				h += "<div><a title='You need to authorise WiDaR to edit" 
+				  + "on your behalf if you want this tool to edit Wikidata.' target='_blank' href='/widar/index.php?action=authorize'>WiDaR</a><br/>not authorised.</div>" ;
+			} else {
+				h += "<div>Logged into <a title='WiDaR authorised' target='_blank'" 
+				  +  "href='/widar/'>WiDaR</a> as <span class='username'>" + d.result.query.userinfo.name + "</span></div>" ;
+				$.each ( d.result.query.userinfo.groups , function ( k , v ) {
+					if ( v != 'bot' ) return ;
+					h += "<div><b>You are a bot</b>, no throttling for you!</div>" ;
+				} ) ;
+				window.clearTimeout(self.oauthTimeout);
+			}
+			$('.oauth_status').html ( h ) ;
+			// $.each ( (((((d||{}).result||{}).query||{}).userinfo||{}).groups||[]) , function ( k , v ) {
+			// 	if ( v == 'bot' ) {
+			// 		max_widar_concurrent = 5 ;
+			// 		widar_edit_delay = 1 ;
+			// 	}
+			// } ) ;
+	});
 }
 
 }
