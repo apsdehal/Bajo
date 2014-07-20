db183920045742b19c13ca138743c4492e713122
Amanpreet Singh <apsdehal@gmail.com>
2014-07-20 23:52:12 +0530

    Adds .bowerrc file

--- app/views/home.php
+++ app/views/home.php
@@ -20,6 +20,7 @@
 </script>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="assets/js/jquery.min.js"><\/script>')</script>
+<script src="assets/config/config.js"></script>
 <script src="assets/js/main.js"></script>
 </body>
 </html>
